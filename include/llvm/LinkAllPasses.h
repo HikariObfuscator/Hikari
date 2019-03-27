f18bcf5eb59f1bc07ba47f1711a359e1b81e0a1f
Zhang <403799106@qq.com>
2019-03-27 17:22:54 +0000

    Add Missing Include

--- include/llvm/LinkAllPasses.h
+++ include/llvm/LinkAllPasses.h
@@ -55,6 +55,7 @@
 #include "llvm/Transforms/Utils/SymbolRewriter.h"
 #include "llvm/Transforms/Utils/UnifyFunctionExitNodes.h"
 #include "llvm/Transforms/Vectorize.h"
+#include "llvm/Transforms/Obfuscation/Obfuscation.h"
 #include <cstdlib>
 
 namespace {
